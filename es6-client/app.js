69f352b8df9f4e4ffd7b3b8d8911e30123ecc433
loyuehei <logap@me.com>
2016-04-12 13:05:37 +0800

    Add babel transpile for client side script

---
+++ es6-client/app.js
@@ -0,0 +1,173 @@
+var socket = io();
+
+const USER_STATE = {
+  INIT: 0,
+  WAITING: 1,
+  PLAYING: 2
+}
+
+const RESULTS = {
+  CREATED:1,
+  JOINED: 2,
+  ROOM_FULL: 3,
+  ROOM_NOT_FOUND: 4,
+  ROOM_EXIST:5,
+  ALL_DONE: 6,
+  PARTNER_READY: 7
+}
+
+var user = {
+  state: USER_STATE.INIT,
+  token: undefined,
+  sex: undefined
+}
+
+
+$('#m-create').click(() => {
+  const roomName = $('#room-id').val();
+  if(user.state == USER_STATE.INIT) {
+    createRoom(roomName,'M')
+  } else {
+    alert("You are in a room already")
+  }
+
+})
+
+$('#f-create').click(() => {
+  const roomName = $('#room-id').val();
+  if(user.state == USER_STATE.INIT) {
+    createRoom(roomName,'F')
+  } else {
+    alert("You are in a room already")
+  }
+
+})
+
+$('#join').click(() => {
+
+  const roomName = $('#room-id').val()
+
+  $.get(`/join/${roomName}`).then((resp) => {
+    const apiResult = resp.result
+    const token = resp.token
+
+    if(apiResult === RESULTS.JOINED && user.state === USER_STATE.INIT) {
+
+      $('#log').append("Joined the room") 
+
+      // save the token
+      user.token = token
+
+      user.sex = oppositeSex(token.substring(0,1))
+
+      // change state to PLAYING
+      user.state = USER_STATE.PLAYING
+
+      showInputBySex(user.sex)
+
+      // subscribe the rooms
+      socket.on(`rooms/${token}`, (jsonStr) => {
+        const wsResp = JSON.parse(jsonStr)
+        console.log(wsResp)
+
+        switch (wsResp.result) {
+          case RESULTS.ALL_DONE:
+            $('#pppd').html(wsResp.paakpaakDay)
+
+            break;
+          case RESULTS.PARTNER_READY:
+            $('#partner-ready-notice').html(`${detailsSex(wsResp.sex)} is ready`)
+        }
+      })
+    } else if(apiResult === RESULTS.ROOM_NOT_FOUND){
+      alert("Room not found!")
+    }
+  })
+
+})
+
+
+$('#ready').click(() => {
+  if(user.state === USER_STATE.PLAYING && user.sex){
+    socket.emit(`rooms`,JSON.stringify({
+      sex: user.sex,
+      m: {
+        mark: $('#m-mark').val(),
+        prop: $('#m-prop').val()
+      },
+      f: {
+        age: $('#f-age').val(),
+        mark: $('#f-mark').val(),
+        exp: $('#f-exp').val()
+      },
+      token: user.token
+    }))
+
+    $('#ready').attr('disabled','disabled')
+  }
+
+})
+
+function oppositeSex(sex) {
+  return sex === 'M'? 'F' : 'M'
+}
+
+function detailsSex(sex){
+  return sex === 'M' ? 'Male' : 'Female'
+}
+
+function createRoom(roomName,sex) {
+    $.get(`/rooms/${roomName}/${sex}`).then((resp) => {
+      const apiResult = resp.result
+      const token = resp.token
+
+      if(apiResult === RESULTS.CREATED) {
+
+        $('#log').append("Room created! Please wait other to come in!")
+
+        // save the token
+        user.token = token
+
+        user.sex = token.substring(0,1)
+
+        // change state to WAITING
+        user.state = USER_STATE.WAITING
+
+        // subscribe the rooms
+        socket.on(`rooms/${token}`, (jsonStr) => {
+          const wsResp = JSON.parse(jsonStr)
+
+          console.log(wsResp)
+
+          switch (wsResp.result) {
+            case RESULTS.JOINED:
+              $('#log').append("Partner has joined the room") 
+
+              user.state = USER_STATE.PLAYING
+
+              showInputBySex(user.sex)
+
+              break;
+
+            case RESULTS.PARTNER_READY:
+              $('#partner-ready-notice').html(`${detailsSex(wsResp.sex)} is ready`)
+
+              break
+            case RESULTS.ALL_DONE:
+              $('#pppd').html(wsResp.paakpaakDay)
+              break;
+          }
+        })
+      } else if(apiResult === RESULTS.ROOM_EXIST){
+        alert("Room name already exist!")
+      }
+    })
+}
+
+function showInputBySex(sex){
+  if(sex == 'M'){
+    $('.female').show();
+  } else {
+    $('.male').show();
+  }
+}
--- public/app.js
+++ public/app.js
@@ -1,87 +1,92 @@
+'use strict';
+
 var socket = io();
 
-const USER_STATE = {
+var USER_STATE = {
   INIT: 0,
   WAITING: 1,
   PLAYING: 2
-}
+};
 
-const RESULTS = {
-  CREATED:1,
+var RESULTS = {
+  CREATED: 1,
   JOINED: 2,
   ROOM_FULL: 3,
   ROOM_NOT_FOUND: 4,
-  ROOM_EXIST:5,
+  ROOM_EXIST: 5,
   ALL_DONE: 6,
   PARTNER_READY: 7
-}
+};
 
 var user = {
   state: USER_STATE.INIT,
   token: undefined,
   sex: undefined
-}
+};
 
-
-$('#m-create').click(() => {
-  const roomName = $('#room-id').val();
-  if(user.state == USER_STATE.INIT) {
-    createRoom(roomName,'M')
+$('#m-create').click(function () {
+  var roomName = $('#room-id').val();
+  if (user.state == USER_STATE.INIT) {
+    createRoom(roomName, 'M');
+  } else {
+    alert("You are in a room already");
   }
+});
 
-})
-
-$('#f-create').click(() => {
-  const roomName = $('#room-id').val();
-  if(user.state == USER_STATE.INIT) {
-    createRoom(roomName,'F')
+$('#f-create').click(function () {
+  var roomName = $('#room-id').val();
+  if (user.state == USER_STATE.INIT) {
+    createRoom(roomName, 'F');
+  } else {
+    alert("You are in a room already");
   }
+});
 
-})
+$('#join').click(function () {
 
-$('#join').click(() => {
+  var roomName = $('#room-id').val();
 
-  const roomName = $('#room-id').val()
+  $.get('/join/' + roomName).then(function (resp) {
+    var apiResult = resp.result;
+    var token = resp.token;
 
-  $.get(`/join/${roomName}`).then((resp) => {
-    const apiResult = resp.result
-    const token = resp.token
+    if (apiResult === RESULTS.JOINED && user.state === USER_STATE.INIT) {
 
-    if(apiResult === RESULTS.JOINED && user.state === USER_STATE.INIT) {
+      $('#log').append("Joined the room");
 
       // save the token
-      user.token = token
+      user.token = token;
 
-      user.sex = oppositeSex(token.substring(0,1))
+      user.sex = oppositeSex(token.substring(0, 1));
 
       // change state to PLAYING
-      user.state = USER_STATE.PLAYING
+      user.state = USER_STATE.PLAYING;
 
-      showInputBySex(user.sex)
+      showInputBySex(user.sex);
 
       // subscribe the rooms
-      socket.on(`rooms/${token}`, (jsonStr) => {
-        const wsResp = JSON.parse(jsonStr)
-        console.log(wsResp)
+      socket.on('rooms/' + token, function (jsonStr) {
+        var wsResp = JSON.parse(jsonStr);
+        console.log(wsResp);
 
         switch (wsResp.result) {
           case RESULTS.ALL_DONE:
-            $('#pppd').html(wsResp.paakpaakDay)
+            $('#pppd').html(wsResp.paakpaakDay);
 
             break;
           case RESULTS.PARTNER_READY:
-            $('#partner-ready-notice').html(`${detailsSex(wsResp.sex)} is ready`)
+            $('#partner-ready-notice').html(detailsSex(wsResp.sex) + ' is ready');
         }
-      })
+      });
+    } else if (apiResult === RESULTS.ROOM_NOT_FOUND) {
+      alert("Room not found!");
     }
-  })
+  });
+});
 
-})
-
-
-$('#ready').click(() => {
-  if(user.state === USER_STATE.PLAYING && user.sex){
-    socket.emit(`rooms`,JSON.stringify({
+$('#ready').click(function () {
+  if (user.state === USER_STATE.PLAYING && user.sex) {
+    socket.emit('rooms', JSON.stringify({
       sex: user.sex,
       m: {
         mark: $('#m-mark').val(),
@@ -93,67 +98,70 @@ $('#ready').click(() => {
         exp: $('#f-exp').val()
       },
       token: user.token
-    }))
+    }));
 
-    $('#ready').attr('disabled','disabled')
+    $('#ready').attr('disabled', 'disabled');
   }
-
-})
+});
 
 function oppositeSex(sex) {
-  return sex === 'M'? 'F' : 'M'
+  return sex === 'M' ? 'F' : 'M';
 }
 
-function detailsSex(sex){
-  return sex === 'M' ? 'Male' : 'Female'
+function detailsSex(sex) {
+  return sex === 'M' ? 'Male' : 'Female';
 }
 
-function createRoom(roomName,sex) {
-    $.get(`/rooms/${roomName}/${sex}`).then((resp) => {
-      const apiResult = resp.result
-      const token = resp.token
+function createRoom(roomName, sex) {
+  $.get('/rooms/' + roomName + '/' + sex).then(function (resp) {
+    var apiResult = resp.result;
+    var token = resp.token;
 
-      if(apiResult === RESULTS.CREATED) {
+    if (apiResult === RESULTS.CREATED) {
 
-        // save the token
-        user.token = token
+      $('#log').append("Room created! Please wait other to come in!");
 
-        user.sex = token.substring(0,1)
+      // save the token
+      user.token = token;
 
-        // change state to WAITING
-        user.state = USER_STATE.WAITING
+      user.sex = token.substring(0, 1);
 
-        // subscribe the rooms
-        socket.on(`rooms/${token}`, (jsonStr) => {
-          const wsResp = JSON.parse(jsonStr)
+      // change state to WAITING
+      user.state = USER_STATE.WAITING;
 
-          console.log(wsResp)
+      // subscribe the rooms
+      socket.on('rooms/' + token, function (jsonStr) {
+        var wsResp = JSON.parse(jsonStr);
 
-          switch (wsResp.result) {
-            case RESULTS.JOINED:
-              user.state = USER_STATE.PLAYING
+        console.log(wsResp);
 
-              showInputBySex(user.sex)
+        switch (wsResp.result) {
+          case RESULTS.JOINED:
+            $('#log').append("Partner has joined the room");
 
-              break;
+            user.state = USER_STATE.PLAYING;
 
-            case RESULTS.PARTNER_READY:
-              $('#partner-ready-notice').html(`${detailsSex(wsResp.sex)} is ready`)
+            showInputBySex(user.sex);
 
-              break
-            case RESULTS.ALL_DONE:
-              $('#pppd').html(wsResp.paakpaakDay)
-              break;
-          }
-        })
-      } else if(apiResult === RESULTS.ROOM_EXIST){
-        alert("Room name already exist!")
-      }
-    })
+            break;
+
+          case RESULTS.PARTNER_READY:
+            $('#partner-ready-notice').html(detailsSex(wsResp.sex) + ' is ready');
+
+            break;
+          case RESULTS.ALL_DONE:
+            $('#pppd').html(wsResp.paakpaakDay);
+            break;
+        }
+      });
+    } else if (apiResult === RESULTS.ROOM_EXIST) {
+      alert("Room name already exist!");
+    }
+  });
 }
 
-function showInputBySex(sex){
-  if(sex == 'M'){
+function showInputBySex(sex) {
+  if (sex == 'M') {
     $('.female').show();
   } else {
     $('.male').show();
